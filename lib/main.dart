32f574db792169e5bd23aea1e95f6b7a63423fbb
Aswin K <aswinvtk2001@gmail.com>
2024-05-30 12:33:12 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+
+import 'package:assessments/screens/quiz_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp( const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: QuizScreen(),),
+  );
+}
---
+++ lib/widgets/title.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TitleShareInfo extends StatelessWidget {
+  const TitleShareInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
